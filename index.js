77cca4337e10ca2437c7f04077f9c8cb2706c222
Saad Ullah <sullah11011@gmail.com>
2024-03-31 15:24:26 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+//operators
+//arithmetic
+//-addition
+let x = 2;
+let y = 5;
+let add = x + y;
+let add1 = "x" + "y";
+let add2 = "xy" + x + y;
+let add3 = "xy" + x + y;
+let add4 = x + y + "xy";
+let add5 = x + y + "xy" + x;
+let add6 = x + "xy";
+let add7 = "xy" + x;
+
+console.log("add:", add);
+console.log("add1:", add1);
+console.log("add2:", add2);
+console.log("add3:", add3);
+console.log("add4:", add4);
+console.log("add5:", add5);
+console.log("add6:", add6);
+console.log("add7:", add7);
+//comparison
+
+//-exponention
